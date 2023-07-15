ac6cc31dfb9aed9861538e0a245c78626ec45104
nateee02 <nathchesim02@gmail.com>
2023-07-15 17:12:14 -0300

    novo commit

--- frontend/script.js
+++ frontend/script.js
@@ -5,13 +5,14 @@ function ler(){
     xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function(){
+        var obj = [];
         obj = JSON.parse( this.responseText );
         obj.forEach( dado => {
-            if (document.getElementById("p" + dado.id) == null){
+            if (document.getElementById("p" + dado.id_receita) == null){
 
                 // Cria um elemento de parágrafo para cada produto
                 const paragraph = document.createElement("p");
-                paragraph.id = "p" + dado.id;
+                paragraph.id = "p" + dado.id_receita;
                 paragraph.innerHTML = `id_receita ${dado.id_receita} - nome_receitas ${dado.nome_receitas} - modo_preparo ${dado.modo_preparo} - fk_categoria ${dado.fk_categoria}`;
           
                 // Adiciona o elemento de parágrafo à div de produtos
@@ -21,7 +22,7 @@ function ler(){
                 const button = document.createElement("button");
                 button.innerHTML = "Excluir";
                 button.addEventListener("click", function() {
-                    excluir(dado.id);
+                    excluir(dado.id_receita);
                 });
           
                 // Adiciona o botão à div de produtos
@@ -37,3 +38,42 @@ function ler(){
       xhttp.open("GET", "http://localhost:8001/receitas", true);
       xhttp.send();
     }
+
+// function ler(){
+//      divJsonInfo = document.getElementById("json-info");
+//      divStatus.innerHTML = "carregando...";
+//      tabela = document.getElementById("receitas");
+    
+//          xhttp = new XMLHttpRequest();
+    
+//          xhttp.onreadystatechange = function(){
+//              if( this.readyState == 4 && this.status == 200){
+//                  obj = JSON.parse( this.responseText );
+//                  obj.forEach( dado => {
+//                      if( document.getElementById("p" + dado.id_receita ) == null ){
+//                          index = tabela.rows.length
+//                          row = tabela.insertRow(-1);
+//                          row.id_receita = "p" + dado.id_receita;
+//                          cellID = row.insertCell(0);
+//                          cellNOME = row.insertCell(1);
+//                          cellPREPARO = row.insertCell(2);
+//                          cellFK_CATEGORIA = row.insertCell(3);
+//                          cellID.innerHTML = dado.id_receita;
+//                          cellNOME.innerHTML = dado.nome_receitas;
+//                          cellPREPARO.innerHTML = dado.modo_preparo;
+//                          cellFK_CATEGORIA.innerHTML = dado.fk_categoria;
+//                      }
+//                  });
+//                  divJsonInfo.innerHTML = "";
+//              }
+    
+//              if( this.readyState == 4 && this.status != 200){
+//                  divJsonInfo.innerHTML = this.responseText;
+//              }
+    
+//          };
+    
+    
+//          xhttp.open("GET" , "http://localhost:8001/receitas", true);
+//          xhttp.send();
+//      }
